042d0c999d84c61c4897c00fd3af0e42d0bb9dca
Victoria Smirnova <vikusha41j@gmail.com>
2017-11-21 08:06:58 +0300

    admi panel, design

--- client/assets/js/controllers/admin.js
+++ client/assets/js/controllers/admin.js
@@ -1,5 +1,5 @@
-var link = document.querySelector('.head').getElementsByTagName('li');
-link.onclick = function () {
-    console.log('.................',123);
-    link.classList.add('select');
-};
+// var link = document.querySelector('.head').getElementsByTagName('li');
+// link.onclick = function () {
+//     console.log('.................',123);
+//     link.classList.add('select');
+// };
